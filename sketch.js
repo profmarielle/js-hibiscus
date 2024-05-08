3014e8bb79f929acd81a7073b85afe257301c646
Marielle Gabriel <mariellegabriel@prof.educacao.sp.gov.br>
2024-05-08 14:24:15 -0300

    alterando o nome do arquivo html

--- sketch.js
+++ sketch.js
@@ -1,34 +1,32 @@
-let palavra;
+let cor;
+let circuloX; // horizontal
+let circuloY; // vertical
 
 function setup() {
   createCanvas(400, 400);
-
-  palavra = palavraAleatoria();
-}
-
-function palavraAleatoria(){
-  let palavras = ["Caminhante", "Caminho", "Caminha"];
-  return random(palavras);
-}
-
-function inicializaCores() {
-
-  background("white");
-  fill("black");
-  textSize(64);
-  textAlign(CENTER, CENTER);
-}
-
-function palavraParcial(minimo, maximo) {
-  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
-  let parcial = palavra.substring(0, quantidade);
-  return parcial;
+  background(color(100, 0 , 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  
+  circuloX = [0, 0, 0];
+  circuloY = [random(height), random(height), random(height)];
 }
 
 function draw() {
-
-  inicializaCores();
-
-  let texto = palavraParcial(0, width);
-  text(texto, 200, 200);
+  
+  fill(cor);
+  
+  for(let contador in circuloX) {
+    circle(circuloX[contador], circuloY[contador], 50);    
+    circuloX[contador]+= random(0,3);
+    circuloY[contador]+= random(-3,3); 
+    
+    if(circuloX[contador] >= width){
+      circuloX[contador] = 0;
+      circuloY[contador] = random(height);
+    }
+  }
+  
+  if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
 }
